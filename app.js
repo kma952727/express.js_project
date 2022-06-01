07cea9ce6b853c7f60651075e2b03fde2b092e38
rara2 <80996897+kma952727@users.noreply.github.com>
2022-06-01 23:20:14 +0900

    * 배포자동화 종료 *

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const passportProvider = require('./middleware/passport.provider');
 const blockHttpMethod = require('./middleware/block.http.method');
 const logRequestTime = require('./middleware/time.middleware');
 require('./config/passport.config')(passport);
-app.use((req, res, next) => console.log('Catch Request'));
+
+app.use(bodyParser.json());
 app.use(blockHttpMethod);
 app.use(logRequestTime);
-app.use(bodyParser.json());
 app.use(passport.initialize());
 app.post('/login',passportProvider.login);
-app.use(passportProvider.verifiedJWT);
 app.use('/users', require('./routes/user.route'));
+app.use(passportProvider.verifiedJWT);
 app.use('/users', require('./routes/movie.shopping.route'))
 app.use('/movies', require('./routes/movie.route'));
 app.use((err, req, res, next) => res.status(err.httpStatus).send(err));
